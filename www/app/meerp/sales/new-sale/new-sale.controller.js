cf4209307cbae75959569c79e01e2135d92fa4de
rrgarciach <rrgarciach@gmail.com>
2016-03-13 02:09:41 -0600

    Added inline comments for new sale controller and template

--- www/app/meerp/sales/new-sale/new-sale.controller.js
+++ www/app/meerp/sales/new-sale/new-sale.controller.js
@@ -3,23 +3,15 @@
 
   angular
     .module('app.meerp.sales')
-    .controller('NewSaleCtrl', [
-      '$location',
-      '$ionicPopup',
-      '$stateParams',
-      'currentSaleService',
-      'productsService',
-      NewSaleCtrl
-    ]);
+    .controller('NewSaleCtrl', NewSaleCtrl);
 
   function NewSaleCtrl($location,
-                             $ionicPopup,
-                             $stateParams,
-                             currentSaleService,
-                             productsService) {
+                       $ionicPopup,
+                       $stateParams,
+                       currentSaleService,
+                       productsService) {
 
     var vm = this;
-
     vm.captureProduct = captureProduct;
 
     init(); // Initialize controller
@@ -30,8 +22,11 @@
     function captureProduct() {
       productsService.getProductsBySKU(vm.productCode)
         .then(function (product) {
+          // Persist products in current sale
           currentSaleService.setProduct(product);
+          // Also update controller's sale object
           vm.sale.products = currentSaleService.getProducts();
+          // Clear input value to be ready for next capture
           vm.productCode = '';
         })
         .catch(function (err) {
@@ -53,14 +48,18 @@
      * Initializes controller setups.
      */
     function init() {
-      vm.sale = {};
+      vm.sale = {}; // Current controller's sale object
+      // Retrieve client from current sale service.
       vm.sale.client = currentSaleService.getClient();
+
       // Check if current sale has not a client selected yet
       if (!vm.sale.client && $stateParams.onsite) {
         // Redirect to locate a client using map
         $location.path('app/clients/locate');
+        // Display instructions
         showAlertOnSiteInstructions();
       }
+
     }
 
   }
